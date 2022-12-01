b238836f549dcea6366784ec27998f16d6884256
Yaz Burrell <yazburrell@Yazs-MacBook-Pro.local>
2022-12-01 13:41:56 -0500

    Setting up log for dates

---
+++ SwiftyPrint/SwiftLogBot.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftLogBot.swift
+//  SwiftyPrint
+//
+//  Created by Yaz Burrell on 12/1/22.
+//
+
+import Foundation
+
+// Debugging dates
+
+enum LogDateFormatter: String {
+    case MM_dd_yyyy_HH_mm_ss_SSS = "MM/dd/yyyy HH:mm:ss:SSS"
+    case MM_dd_yyyy_HH_mm_ss = "MM-dd-yyyy HH:mm:ss"
+    case E_d_MMM_yyyy_HH_mm_ss_Z = "E, d MMM yyyy HH:mm:ss:SSSZ"
+    case MMM_d_HH_mm_ss_SSSZ
+
+}
---
+++ SwiftyPrint/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftyPrint
+//
+//  Created by Yaz Burrell on 12/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("This is a standard print statement")
+    }
+
+
+}
+
